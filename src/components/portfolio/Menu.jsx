2f810231ff5bba7a4e8eebccd0177775d6000419
Rane-19 <dia1929@yahoo.com.ph>
2023-11-16 16:04:07 -0800

    improved Recent Works and changed background

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import Work1 from "../../assets/work-1.svg";
+import Work2 from "../../assets/work-2.svg";
+import Work3 from "../../assets/work-3.svg";
+import Work4 from "../../assets/work-4.svg";
+import Work5 from "../../assets/work-5.svg";
 
-const Menu = () => {
-  return (
-    <div>Menu</div>
-  )
-}
+const Menu = [
+    {
+        id: 1,
+        image: Work1,
+        title: "Project Management Illustration",
+        category: "Design",
+    },
+    {
+        id: 2,
+        image: Work2,
+        title: "Guest App Walkthrough Screens",
+        category: "Art",
+    },
+    {
+        id: 3,
+        image: Work3,
+        title: "Delivery App Wireframe",
+        category: "Branding",
+    },
+    {
+        id: 4,
+        image: Work4,
+        title: "Project Management Illustration",
+        category: "Design",
+    },
+    {
+        id: 5,
+        image: Work5,
+        title: "Project Management Illustration",
+        category: "Design",
+    },
+]
 
 export default Menu
