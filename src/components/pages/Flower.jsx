9f65e422beac228451141222d32bb97c90cc6b58
Rane-19 <dia1929@yahoo.com.ph>
2023-11-18 15:28:09 -0800

    finished the mobile version except for the single page

---
+++ src/components/pages/Flower.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './pages/pages.css';
+
+const Flower = () => {
+
+}
+
+export default Flower
---
+++ src/components/pages/Game.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './pages/pages.css';
+
+const Game = () => {
+    return (
+        
+    )
+}
+
+export default Game
